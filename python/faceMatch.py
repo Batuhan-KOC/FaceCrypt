8d3e2e1a5356c51cb740afc44aa90cdbe246a802
Batuhan-KOC <1997.batuhankoc@gmail.com>
2023-06-03 13:55:13 +0300

    Face match python file introduced

---
+++ python/faceMatch.py
@@ -0,0 +1,53 @@
+import getopt, sys
+
+argumentList = sys.argv[1:]
+
+options = "p:"
+
+longOptions = ["image1=", "image2=", "port="]
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, longOptions)
+
+    if True:
+        im1Path = ""
+        im2Path = ""
+        portNumber = ""
+
+        for argument, value in arguments:
+            if argument in ("--image1"):
+                im1Path = value
+            elif argument in ("--image2"):
+                im2Path = value
+            elif argument in ("-p", "--port"):
+                portNumber = value
+
+        if((len(im1Path) == 0 | im1Path == ' ') | (len(im2Path) == 0 | im2Path == ' ') | (len(portNumber) | portNumber == ' ') == 0):
+            # output error
+            print ("Invalid argument list error")
+        else :
+            print(("image 1 path    : %s") % im1Path)
+            print(("image 2 path    : %s") % im2Path)
+            print(("port number is  : %s") % portNumber)
+            print("Importing dynamic libraries..")
+
+            from deepface import DeepFace
+            import socket
+
+            print("Initializing udp socket..")
+            UDP_IP = "127.0.0.1"
+            UDP_PORT = int(portNumber)
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.bind((UDP_IP, UDP_PORT))
+
+            print("Verifying images..")
+            result = DeepFace.verify(img1_path = im1Path, img2_path = im2Path)
+            
+    else :
+        # output error
+        print ("Invalid argument list")
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
